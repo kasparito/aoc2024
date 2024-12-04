e8cc96d06df26fd240148aa5f1bdcc4b50c01c66
kaspar <kaspar@hellden.com>
2024-12-04 23:24:16 +0100

    More Grid improvements

--- src/com/hellden/grid/Grid.scala
+++ src/com/hellden/grid/Grid.scala
@@ -9,10 +9,13 @@ class Grid[T](grid: IndexedSeq[IndexedSeq[T]]):
   case class Cell(position: Position, value: T):
 
     def move(direction: Direction, steps: Int = 1): Option[Cell] =
-      at(position.move(direction, steps))
+      cellAt(position.move(direction, steps))
+
+    def cellsIn(direction: Direction, offset: Int = 0): LazyList[Cell] =
+      cellsFrom(position, direction, offset)
 
     def valuesIn(direction: Direction, offset: Int = 0): LazyList[T] =
-      valuesFrom(position, direction, offset)
+      cellsIn(direction, offset).map(_.value)
 
   val rows: IndexedSeq[IndexedSeq[Cell]] =
     grid.zipWithIndex.map: (row, y) =>
@@ -21,22 +24,14 @@ class Grid[T](grid: IndexedSeq[IndexedSeq[T]]):
 
   def cells: Iterable[Cell] = rows.view.flatten
 
-  def at(position: Position): Option[Cell] =
+  def cellAt(position: Position): Option[Cell] =
     for
       row <- rows.lift(position.y)
       value <- row.lift(position.x)
     yield value
 
-  def valuesFrom(position: Position, direction: Direction, offset: Int = 0): LazyList[T] =
-    LazyList
-      .from(offset)
-      .map: steps =>
-        position.move(direction, steps)
-      .map(at)
-      .takeWhile(_.isDefined)
-      .collect:
-        case Some(cell) =>
-          cell.value
+  def cellsFrom(position: Position, direction: Direction, offset: Int = 0): LazyList[Cell] =
+    LazyList.from(offset).map(position.move(direction, _)).map(cellAt).takeWhile(_.isDefined).flatten
 
   def find(value: T): Iterable[Cell] =
     cells.filter(_.value == value)
